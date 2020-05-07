f7dba32283e98c9c2003de81121d6b6b44843038
huequica <harumaki1225@gmail.com>
2020-05-07 18:04:01 +0900

    レンダリングをさせてみる

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
 import Vue from 'vue';
+import Root from './Root.vue';
 
 Vue.config.productionTip = false;
+
+// eslint-disable-next-line no-new
+new Vue({
+	render: (h) => h(Root),
+	el: 'div#app',
+});
