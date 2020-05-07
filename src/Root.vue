f0ba53282ee4f84d54beb50d6c8bca317e525d57
huequica <harumaki1225@gmail.com>
2020-05-07 18:15:14 +0900

    動的レンダリングを追加

--- src/Root.vue
+++ src/Root.vue
@@ -1,5 +1,19 @@
 <template>
 	<div>
-		Hello Vue!
+		Hello Vue!<br />
+		{{ message }}
 	</div>
 </template>
+
+<script>
+export default {
+	name: 'Root',
+
+	// VueにおいてArrow Functionは非推奨ぽい？
+	data() {
+		return {
+			message: 'Hello World',
+		};
+	},
+};
+</script>
