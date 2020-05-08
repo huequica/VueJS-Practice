d9754b4536ff828c4b1b37c797d14c0a3581de36
huequica <harumaki1225@gmail.com>
2020-05-08 16:07:06 +0900

    buttonコンポーネントを作成

---
+++ src/Components/button.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<p></p>
+		<button v-on:click="reverseMessage">ReverseMessage</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'button',
+	data() {
+		return {
+			Message: 'Reversed!'
+		};
+	},
+
+	// `methods.~~` でイベントハンドラ等を記述
+	// この際自分のコンポーネントの `state` は `this.~~` で拾える
+	methods: {
+		reverseMessage() {
+			this.message = this.message.split('')
+				.reverse()
+				.join();
+		}
+	}
+};
+</script>
