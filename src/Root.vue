b55c6505778c07d2b34a718348102b4f86191d2a
huequica <harumaki1225@gmail.com>
2020-05-08 15:08:32 +0900

    ループで `<li>` を作る処理の実装

--- src/Root.vue
+++ src/Root.vue
@@ -1,12 +1,20 @@
 <template>
 
 	<div v-bind:title="title">
-		Hello Vue!<br />
+		Hello Vue!<br/>
 		{{ message }}
 
 		<span v-if="isSpanAppear">
 			<code>isSpanAppear</code>は<code>true</code>だよ
 		</span>
+
+		{{/* `v-for` でeachみたいな処理が可能、ただし `v-bind:key` を与える必要あり */}}
+		<ol>
+			<li v-for="Item in ListQue" v-bind:key="Item">
+				{{Item.text}}
+			</li>
+		</ol>
+
 	</div>
 
 </template>
