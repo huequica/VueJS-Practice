c45c3ef5e28dfb35955baf60aebe42f5e7ba0628
huequica <harumaki1225@gmail.com>
2020-05-14 14:06:50 +0900

    コンポーネント作成

---
+++ src/Components/model.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<p>{{ message }}</p>
+		<label>
+			<input v-model="message" />
+		</label>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Model',
+	data() {
+		return {
+			message: 'Hello Vue!'
+		};
+	}
+};
+</script>
